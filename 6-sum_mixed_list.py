72fe34c3c6865cfa4e65370caceef0fbcacad467
kc-clintone <kayseeclintone@gmail.com>
2024-08-01 13:47:15 +0000

    task 6 - basic operations

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Intro to complex types - mixed list
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    This computes the sum of a list of integers and floating-point numbers.
+    """
+    return float(sum(mxd_lst))
