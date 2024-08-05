038a661dc1232411ca2f56cba612ee81e56f4a52
kc-clintone <kayseeclintone@gmail.com>
2024-08-05 07:30:22 +0000

    task 4: Still running tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Still running tasks
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This executes task_wait_random n number of times.
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
