9e83849a15e67bd126af8a7a48905c62829eeb7f
kc-clintone <kayseeclintone@gmail.com>
2024-08-05 07:27:48 +0000

    task 3: Running tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Running tasks
+"""
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Spawns asynchronous task for wait_random.
+    """
+    return asyncio.create_task(wait_random(max_delay))
