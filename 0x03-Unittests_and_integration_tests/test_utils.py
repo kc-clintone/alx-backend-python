67a1e48a83aec07eb62a8a8aec515077fcb1aede
kc-clintone <kayseeclintone@gmail.com>
2024-08-25 10:12:43 +0000

    Task 1: Unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -32,13 +32,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """
-        Test access_nested_map to ensure a KeyError is raised for invalid paths.
+        Test access_nested_map to ensure a KeyError is raised for invalid
+        paths.
 
         Parameters:
         - nested_map: The dictionary to attempt to retrieve the value from.
@@ -52,6 +54,7 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(context.exception), str(path[-1]))
+
 
 if __name__ == "__main__":
     unittest.main()
