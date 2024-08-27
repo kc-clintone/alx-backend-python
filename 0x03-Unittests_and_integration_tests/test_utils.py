35341f446be2998ff42f3d6dbce01b6bb010b82c
kc-clintone <kayseeclintone@gmail.com>
2024-08-27 06:26:51 +0000

    Task 2: Unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Unittests Module
+Testing module
 """
 
 
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
 
 from typing import (
     Dict,
@@ -25,29 +25,33 @@ from utils import (
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Test case for the utils.access_nested_map function.
+    Test case for utils.access_nested_map function.
     """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            expected: Union[Dict, int]
+            ) -> None:
         """
         Test access_nested_map with different nested maps and paths.
 
         Parameters:
         - nested_map: The dictionary to retrieve the value from.
-        - path: A tuple representing the sequence of keys to access the value.
+        - path: A tuple representing the sequence of keys to access the
+        value.
         - expected: The expected result after accessing the nested map.
 
         The method checks if the value returned by access_nested_map
         matches the expected result.
         """
-
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
 
     @parameterized.expand([
         ({}, ("a",), KeyError),
@@ -59,7 +63,6 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Sequence,
             exception: Exception,
             ) -> None:
-
         """
         Test access_nested_map to ensure a KeyError is raised for
         invalid
@@ -77,10 +80,8 @@ class TestAccessNestedMap(unittest.TestCase):
         the nested map.
         Also verifies that the exception message is as expected.
         """
-
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
-
 
 
 class TestGetJson(unittest.TestCase):
@@ -90,10 +91,13 @@ class TestGetJson(unittest.TestCase):
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url, test_payload):
-
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
         """
         Test that get_json returns the correct JSON payload from
         the given URL.
@@ -109,16 +113,10 @@ class TestGetJson(unittest.TestCase):
         - get_json returns the expected test_payload.
         """
 
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
+        attrs = {'json.return_value': test_payload}
 
-        with patch('utils.requests.get',
-                    return_value=mock_response) as mock_get:
-            result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_payload)
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
-
-if __name__ == "__main__":
-    unittest.main()
 
