83fda7b9d749d9525d78edac942856648ae6ad85
kc-clintone <kayseeclintone@gmail.com>
2024-08-27 10:40:47 +0000

    Corrections

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,62 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/users/google/repos",
             )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """
+        Tests the public_repos method.
+        """
+
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 1697149,
+                    "name": "cool-app",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342084,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/cool-app",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 17,
+                    "default_branch": "main",
+                },
+                {
+                    "id": 8560972,
+                    "name": "keratus",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1332094,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/keratus",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 30,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch(
+                "client.GithubOrgClient._public_repos_url",
+                new_callable=PropertyMock,
+                ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    "cool-app",
+                    "keratus",
+                ],
+            )
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
